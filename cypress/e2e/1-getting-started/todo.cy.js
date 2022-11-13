fee651d1cf41fb7159a900d794689933c1db492e
IgaPoplawska <poplawska.iga.anna@gmail.com>
2022-11-13 11:54:45 +0100

    tests: add e2e tests for arkhamDB home page

--- cypress/e2e/1-getting-started/todo.cy.js
+++ cypress/e2e/1-getting-started/todo.cy.js
@@ -12,14 +12,22 @@ describe("arkhamdb smoke tests", () => {
     cy.get(".bg-faction").eq(0).should("exist");
     cy.get(".bg-faction").eq(0).find("a").should("exist");
     cy.get("nav .navbar-brand").should("have.attr", "href", "/");
+    cy.get("nav ul li").eq(0).should("have.text", "My Decks");
+    cy.get("nav ul li").eq(1).should("have.text", "Decklists");
+    cy.get("nav ul li").eq(2).should("have.text", "Cards");
+    cy.get("nav ul li").eq(3).should("have.text", "Reviews");
+    cy.get("nav ul li").eq(4).should("have.text", "Rules");
+    cy.get("nav ul li").eq(5).should("have.text", "FAQs");
+
     cy.get("nav .navbar-form input").type("hehe");
     cy.get("nav .navbar-form input").should("have.value", "hehe");
   });
-  it("should API work correctly", () => {
-    cy.request("/").debug();
-    cy.request("/").then((resp) => {
-      expect(resp.status).to.eq(200);
-      console.log(resp);
-    });
-  });
+
+  // it("should API work correctly", () => {
+  //   cy.request("/").debug();
+  //   cy.request("/").then((resp) => {
+  //     expect(resp.status).to.eq(200);
+  //     console.log(resp);
+  //   });
+  // });
 });
